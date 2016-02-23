f5ec667f9bb1c6fdfc1ff65d16d254a9b882d89a
Johannes Unterstein <unterstein@me.com>
2016-02-23 21:43:44 +0100

    introduced test usage of a repo

--- web/app/controllers/Application.scala
+++ web/app/controllers/Application.scala
@@ -1,11 +1,17 @@
 package controllers
 
-import play.api._
+import javax.inject.{Inject, Named}
+
+import neo4j.models.ExampleObject
+import neo4j.repositories.ExampleRepository
 import play.api.mvc._
 
-class Application extends Controller {
+@Named
+class Application @Inject()(repo: ExampleRepository) extends Controller {
 
   def index = Action {
+    repo.save(new ExampleObject)
+    println(repo.count())
     Ok(views.html.index("Your new application is ready."))
   }
 
